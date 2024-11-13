f14d4fe938ebb73cc675eeb813f726318001bd6d
Samarth Rajput <ssamarth224@gmail.com>
2024-11-13 14:42:54 +0530

    changed frontend appbar

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -3,7 +3,7 @@ import { Avatar } from "./BlogCard"
 export const Appbar = () => {
     return <div className="border-b flex justify-between px-10 py-4">
         <Link to={'/blogs'} className="flex justify-center flex-col cursor-pointer"> 
-            Medium
+            Blog App
         </Link>
         <div>
             <Link to={`/publish`}>
